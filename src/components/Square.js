b76f6c903f82293a9e2429d91ea05e728bab172f
Bhagavathiraja <103630965+Bhagavathiraja@users.noreply.github.com>
2022-04-24 16:17:27 +0530

    basics of XO game

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Square = (pros) => {
-    console.log(pros)
-    return ( 
-    <button type = "button" className= "square ">{pros.value}</button> 
-    );
-}
- 
+const Square = ({ value, onClick }) => {
+  return (
+    <button type="button" className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+};
+
 export default Square;
